b4c846260dc3a79f1e6edfe2d5715f54497d8dfe
mariechanta <52567733+mariechanta@users.noreply.github.com>
2024-03-10 12:24:06 +0100

    commit

--- vite.config.jsx
+++ vite.config.jsx
@@ -1,19 +1,7 @@
-import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { resolve, dirname } from "node:path";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx()
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
-    }
-  },
-  base: "/"
-});
+  plugins: [react()],
+})
