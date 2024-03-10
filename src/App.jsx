2ddec6365b1791eb23727a51a380f0209bdf2391
mariechanta <52567733+mariechanta@users.noreply.github.com>
2024-03-10 12:44:27 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import React, { useState, useEffect } from 'react';
 import StudentList from './StudentList';
 import DataFetchingComponent from './Api';
 import StyledComponent from './Styled';
