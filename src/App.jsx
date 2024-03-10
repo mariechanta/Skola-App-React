19d23df9441d036977b23ee2f58a4f1f723239de
mariechanta <52567733+mariechanta@users.noreply.github.com>
2024-03-10 12:30:04 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 
 import React from 'react';
-import StudentList from './Components/StudentList';
 import DataFetchingComponent from './Components/DataFetchingComponent';
 import StyledComponent from './Components/StyledComponent';
 import UseReducerComponent from './Components/UseReducerComponent';
