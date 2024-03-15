de7d9ef7417e3f30607c0f8f637aec880c817fce
mariechanta <52567733+mariechanta@users.noreply.github.com>
2024-03-15 13:26:04 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  // other configuration options  vite.config.js
-  resolve: {
-    alias: {
-      '@': '/src', // Add aliases for commonly used paths
-    },
-  },
-});
+  plugins: [react()],
+})
