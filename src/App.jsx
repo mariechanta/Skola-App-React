08e4e8e7dadb7fb66697a5bcfc6ba3c33dcb9afb
mariechanta <52567733+mariechanta@users.noreply.github.com>
2024-03-15 12:56:36 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 // src/App.js
 import React from "react";
 import ReactDom from "react -dom/client";
-import App from "./App";
-import StudentList from "./StudentList";
-import Button from "./Button";
-import Counter from "./Counter";
-import "./index.css";
+import App from "/App";
+import StudentList from "/StudentList";
+import Button from "/Button";
+import Counter from "/Counter";
+import "/index.css";
 
 const App = () => {
   return (
