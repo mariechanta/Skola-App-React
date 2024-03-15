92360b61decaba810d7329be3c86f80a846358b7
mariechanta <52567733+mariechanta@users.noreply.github.com>
2024-03-15 13:19:09 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
-  // other configuration options
+  // other configuration options  vite.config.js
   resolve: {
     alias: {
       '@': '/src', // Add aliases for commonly used paths
